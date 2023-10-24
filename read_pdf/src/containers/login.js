05a14f202d6d34b2b584e2c4e50d2915e45b80af
Leo Alcantara <leo.alca.salvador@gmail.com>
2023-10-24 00:35:50 -0300

    checkpoint

---
+++ read_pdf/src/containers/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Login() {
+    return (
+        <div className="Login">
+        <header className="Login-header">
+            <p>
+            Login
+            </p>
+        </header>
+        </div>
+    );
+}
